21dbed9e3729d27dd26df546f6c0d8d7c3b14618
Ronaldo Umana <ronaldo.ur@gmail.com>
2018-11-14 10:32:01 -0600

    Configured for docker

--- public/javascripts/courses.model.js
+++ public/javascripts/courses.model.js
@@ -1,74 +1,8 @@
 const mongodb = require('mongodb')
 const assert = require('assert')
 
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://mongodb:27017'
 const dbName = 'learnHub'
-
-var courses  = [
-   {
-      id: 0,
-      name: 'PHP for dummies',
-      mentor: 'Geovanny Lopez',
-      price: 1.99,
-      imageUrl: '/assets/php.png',
-      releaseDate: new Date('05/06/2018'),
-      level: 'beginer'
-   },
-   {
-      id: 1,
-      name: 'Advanced C++',
-      mentor: 'Ronaldo U',
-      price: 99.98,
-      imageUrl: '/assets/cpp.png',
-      releaseDate: new Date('05/10/2018'),
-      level: 'advanced'
-   },
-   {
-      id: 2,
-      name: 'Assembler',
-      mentor: 'Martin P',
-      price: 85.99,
-      imageUrl: '/assets/asm.png',
-      releaseDate: new Date('07/07/1999'),
-      level: 'advanced'
-   },
-   {
-      id: 3,
-      name: 'Angular Tour',
-      mentor: 'Juanito',
-      price: 50.50,
-      imageUrl: '/assets/angular.png',
-      releaseDate: new Date('06/06/2018'),
-      level: 'intermediate'
-   },
-   {
-      id: 4,
-      name: 'Master React',
-      mentor: 'Paolo',
-      price: 50.50,
-      imageUrl: '/assets/react.png',
-      releaseDate: new Date('06/06/2018'),
-      level: 'intermediate'
-   }
-]
-
-function findPosition(id) {
-   for(let i = 0; i < courses.length; i++) {
-      if(courses[i].id == id) {
-         return i
-      }
-   }
-   return -1
-}
-
-function exist(id) {
-   if(findPosition(id) === -1) {
-      return false
-   }
-   else {
-      return true
-   }
-}
 
 async function getCourse(id) {
    try {
@@ -143,4 +77,4 @@ async function deleteCourse(id) {
    }
 }
 
-module.exports = { getCourse, exist, getCourses, addCourse, editCourse, deleteCourse }
+module.exports = { getCourse, getCourses, addCourse, editCourse, deleteCourse }
